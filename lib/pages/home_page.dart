5576ba5ab2de4bfe4595067da5003620e5956af6
gipeio <gipeio22@gmail.com>
2023-12-28 21:06:56 +0100

    email sign up, email sign in, guest sign in

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,59 +1,196 @@
-import 'package:etarot/test/auth_test.dart';
-import 'package:etarot/test/card_face_service_test.dart';
-import 'package:etarot/test/card_service_test.dart';
-import 'package:etarot/test/drawing_service_test.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: Text('Authentication Page'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            SizedBox(height: 16.0),
             ElevatedButton(
               onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CardTestPage()),
-                );
+                _showSignInDialog();
               },
-              child: Text('cards'),
+              child: Text('Sign In with Email'),
             ),
+            SizedBox(height: 16.0),
             ElevatedButton(
               onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CardFaceServiceTest()),
-                );
+                _showSignUpDialog();
               },
-              child: Text('faces'),
+              child: Text('Sign Up with Email'),
             ),
+            SizedBox(height: 16.0),
             ElevatedButton(
               onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => AuthenticationTestPage()),
-                );
+                _showGuestWarningDialog();
               },
-              child: Text('Auth'),
+              child: Text('Continue as Guest'),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => DrawingTestPage()),
-                );
-              },
-              child: Text('Drawing'),
-            ),
+            // ...rest of your code
           ],
         ),
       ),
     );
   }
+
+  void _showSignInDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Sign In with Email'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                controller: passwordController,
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the dialog
+              },
+              child: Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                try {
+                  await FirebaseAuth.instance.signInWithEmailAndPassword(
+                    email: emailController.text,
+                    password: passwordController.text,
+                  );
+                  print('User signed in successfully!');
+                  Navigator.pop(context); // Close the dialog
+                } catch (e) {
+                  print('Error signing in: $e');
+                }
+              },
+              child: Text('Sign In'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showSignUpDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Sign Up with Email'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                controller: passwordController,
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                controller: confirmPasswordController,
+                decoration: InputDecoration(labelText: 'Confirm Password'),
+                obscureText: true,
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the dialog
+              },
+              child: Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                try {
+                  if (passwordController.text == confirmPasswordController.text) {
+                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                      email: emailController.text,
+                      password: passwordController.text,
+                    );
+                    print('User signed up successfully!');
+                    Navigator.pop(context); // Close the dialog
+                  } else {
+                    print('Passwords do not match');
+                  }
+                } catch (e) {
+                  print('Error signing up: $e');
+                }
+              },
+              child: Text('Sign Up'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showGuestWarningDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Warning'),
+          content: Text('Some functionalities are disabled when logged in as a guest. For the best experience, log in with an account.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the dialog
+              },
+              child: Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                // Add logic to continue as a guest
+                print('User continued as a guest!');
+                Navigator.pop(context); // Close the dialog
+              },
+              child: Text('Continue'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // ...rest of your code
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
 }
