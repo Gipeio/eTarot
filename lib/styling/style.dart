a367fa57ac3c91b6aa99fc1934d3af8b08c7ac13
gipeio <gipeio22@gmail.com>
2024-01-08 19:30:46 +0100

    new colors and new logo

--- lib/styling/style.dart
+++ lib/styling/style.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 class Style { 
 	static const double defaultPadding = 16.0; 
 	static const double defaultFontSize = 16.0; 
-  static const Color itemColor = Color.fromRGBO(255, 180, 180, 1.0);
-  static const Color backgroundColor = Color.fromRGBO(255, 249, 202, 1.0);
+  static const Color backgroundColor = Color.fromRGBO(69, 25, 82, 1.0);
+  static const Color itemColor = Color.fromRGBO(174, 68, 90, 1.0);
+  static const Color itemTextColor = Color.fromRGBO(244, 238, 255,1.0);
   static const Color guestRedirectTextColor = Color.fromRGBO(96, 116, 133, 1.0);
-  static const Color itemTextColor = Color.fromRGBO(178,164,255,1.0);
 }
