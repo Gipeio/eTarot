0a550f904449a1166850b1a634ebb3d2c073a035
gipeio <gipeio22@gmail.com>
2024-01-09 16:25:53 +0100

    full home page

---
+++ lib/pages/draw_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DrawPage extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -1,36 +1,76 @@
 import 'package:etarot/pages/authentification_page.dart';
+import 'package:etarot/pages/draw_page.dart';
+import 'package:etarot/pages/stat_page.dart';
+import 'package:etarot/styling/button_style.dart';
+import 'package:etarot/styling/style.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class MenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        await FirebaseAuth.instance.signOut();
-        return true; // Return true to allow the back button press
-      },
-      child: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Current User: ${FirebaseAuth.instance.currentUser?.email ?? 'Guest'}'),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () async {
-                  await FirebaseAuth.instance.signOut();
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => AuthPage()),
-                  );
-                },
-                child: Text('Logout'),
+    return Scaffold(
+      backgroundColor: Style.backgroundColor,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: MediaQuery.of(context).size.height * 0.15),
+            Text(
+              'Current User: ${FirebaseAuth.instance.currentUser?.email ?? 'Guest'}',
+              style: const TextStyle(
+                fontFamily: 'Rocher',
+                fontSize: 40,
+                color: Style.itemTextColor,
               ),
-            ],
-          ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.25),
+            CustomButtonStyle(label: 'Draw', onPressed: () => _navigateToPage(context, 'Draw')),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+            CustomButtonStyle(label: 'Statistics', onPressed: () => _navigateToPage(context, 'Statistics')),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.175),
+            GestureDetector(
+              onTap: () async {
+                await FirebaseAuth.instance.signOut();
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => AuthPage()),
+                );
+              },
+              child: const Text(
+                'Logout',
+                style: TextStyle(
+                  color: Style.guestRedirectTextColor, // Text color
+                  decoration: TextDecoration.underline, // Optional: Add underline
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
+
+  Future<bool> _onWillPop() async {
+    await FirebaseAuth.instance.signOut();
+    return true; // Return true to allow the back button press
+  }
+
+void _navigateToPage(BuildContext context, String pageName) {
+  switch (pageName) {
+    case 'Draw':
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => DrawPage()),
+      );
+    case 'Statistics':
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => StatPage()),
+      );
+      break;
+    default:
+      print('Unknown page: $pageName');
+  }
+}
+
 }
