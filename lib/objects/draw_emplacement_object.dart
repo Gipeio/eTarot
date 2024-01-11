8af7493a86de39ad07c76564eee3d2dfd048a199
gipeio <gipeio22@gmail.com>
2024-01-11 16:20:23 +0100

    responsive draw first try

--- lib/objects/draw_emplacement_object.dart
+++ lib/objects/draw_emplacement_object.dart
@@ -2,7 +2,6 @@ import 'package:etarot/styling/style.dart';
 import 'package:flutter/material.dart';
 
 class DrawEmplacementObject extends StatelessWidget {
-
   final int index;
 
   const DrawEmplacementObject({
@@ -11,20 +10,21 @@ class DrawEmplacementObject extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return GestureDetector(
-            onTap: () {
-              // Add your functionality here when the rectangle is clicked
-              print('Rectangle ${index} clicked!');
-            },
-            child: Container(
-              margin: EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.black),
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              // You can customize the content of each rectangle here
-            ),
-          );
+      onTap: () {
+        // Add your functionality here when the rectangle is clicked
+        print('Rectangle $index clicked!');
+      },
+      child: Container(
+        margin: EdgeInsets.all(14.0),
+        decoration: BoxDecoration(
+          color: Style.backgroundColor,
+          border: Border.all(color: Style.itemColor),
+          borderRadius: BorderRadius.circular(40.0),
+        ),
+        // You can customize the content of each rectangle here
+      ),
+    );
   }
 }
--- lib/pages/draw_page.dart
+++ lib/pages/draw_page.dart
@@ -1,8 +1,9 @@
 import 'package:etarot/model/Card.dart';
+import 'package:etarot/objects/button_object.dart';
 import 'package:etarot/objects/draw_emplacement_object.dart';
+import 'package:etarot/services/card_service.dart';
 import 'package:etarot/styling/style.dart';
 import 'package:flutter/material.dart';
-import 'package:etarot/services/card_service.dart';
 
 class DrawPage extends StatefulWidget {
   @override
@@ -13,23 +14,42 @@ class _DrawPageState extends State<DrawPage> {
   @override
   void initState() {
     super.initState();
-    loadAndPrintCards();
+    loadCards();
   }
 
-  Future<void> loadAndPrintCards() async {
+  void _nullFunc() {
+    // Add functionality for button press here
+  }
+
+    Future<void> loadCards() async {
     CardService cardService = CardService();
     List<TarotCard> cards = await cardService.getAllCards();
+    for (TarotCard card in cards){
+      print(card.name);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Style.backgroundColor,
-      body: GridView.count(
-        crossAxisCount: 2,
-        children: List.generate(4, (index) {
-          return DrawEmplacementObject(index: index);
-        }),
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.85, // Set a fixed height for GridView
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: MediaQuery.of(context).size.width / (2 * MediaQuery.of(context).size.height * 0.4),
+              ),
+              itemBuilder: (context, index) {
+                return DrawEmplacementObject(index: index);
+              },
+              itemCount: 4,
+            ),
+          ),
+          CustomButtonObject(label: "aeazeaze", onPressed: _nullFunc),
+        ],
       ),
     );
   }
